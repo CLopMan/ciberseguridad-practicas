d82b8f661cd7c17f463a0fbd48216d10d04ca3f6
CLopMan <cesarlopman@gmail.com>
2024-12-18 10:31:49 +0100

    rotate_seed

--- practica3/scripts/rotate_seed.py
+++ practica3/scripts/rotate_seed.py
@@ -2,7 +2,7 @@ seed = "srrqnn"
 prefix = "6931fac9da"
 sufix = "b2b36c248b"
 
-for i in range(27):
+for i in range(26):
     b = ""
     for j in seed:
         b += chr(((ord(j) - 97 + i) % 26) + 97)
